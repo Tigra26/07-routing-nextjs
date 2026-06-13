b8f21cfe33e553729bffc64110c41292c4e731f0
Alena Shykova <shining8smaragd@gmail.com>
2026-06-13 12:40:57 +0300

    Update default.tsx

--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -1,3 +1,20 @@
+import Link from "next/link";
+import css from "./SidebarNotes.module.css";
+
+const tags = ["Todo", "Work", "Personal", "Meeting", "Shopping"];
+
 export default function Default() {
-  return null;
+  return (
+    <aside className={css.sidebar}>
+      <Link href="/notes/filter/all" className={css.link}>
+        All notes
+      </Link>
+
+      {tags.map((tag) => (
+        <Link key={tag} href={`/notes/filter/${tag}`} className={css.link}>
+          {tag}
+        </Link>
+      ))}
+    </aside>
+  );
 }
